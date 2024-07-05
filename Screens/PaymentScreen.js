e46154d8668422a1270b4e6fc4d80d019a991cfc
Mosongo Adina <amosongodina@gmail.com>
2024-07-05 17:46:36 +0100

    Fixing keyboard overlay

--- Screens/PaymentScreen.js
+++ Screens/PaymentScreen.js
@@ -7,14 +7,15 @@ import {
   StyleSheet,
   StatusBar,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import PaymentMethodCard from "../components/PaymentMethodCard";
 import BackArrow from "../assets/arrow-circle-left.png";
-import MtnLogo from '../assets/mtn_momo.png';
-import OrangeLogo from '../assets/orange_money.png';
-import StripeLogo from '../assets/stripe.jpg';
-import VisaLogo from '../assets/visa.png';
+import MtnLogo from "../assets/mtn_momo.png";
+import OrangeLogo from "../assets/orange_money.png";
+import StripeLogo from "../assets/stripe.jpg";
+import VisaLogo from "../assets/visa.png";
 
 const PaymentScreen = ({ route }) => {
   const navigation = useNavigation();
@@ -22,7 +23,7 @@ const PaymentScreen = ({ route }) => {
   const [selectedCard, setSelectedCard] = useState(null);
 
   const handleCardSelect = (cardName) => {
-    setSelectedCard(prevCard => (prevCard === cardName ? null : cardName));
+    setSelectedCard((prevCard) => (prevCard === cardName ? null : cardName));
   };
 
   return (
@@ -30,44 +31,46 @@ const PaymentScreen = ({ route }) => {
       <StatusBar backgroundColor="#070C35" />
       <View style={styles.backgroundDiv}>
         <View style={styles.another}>
-          <TouchableOpacity
-            style={styles.backArrowContainer}
-            onPress={() => navigation.goBack()}
-            accessible={true}
-            accessibilityLabel="Back"
-            accessibilityHint="Navigates to the previous screen"
-          >
-            <Image source={BackArrow} style={styles.backArrow} />
-          </TouchableOpacity>
-          <Text style={styles.header}>Make Payment</Text>
+          <View>
+            <TouchableOpacity
+              style={styles.backArrowContainer}
+              onPress={() => navigation.goBack()}
+              accessible={true}
+              accessibilityLabel="Back"
+              accessibilityHint="Navigates to the previous screen"
+            >
+              <Image source={BackArrow} style={styles.backArrow} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.heading}>
+            <Text style={styles.header}>Make Payment</Text>
+          </View>
         </View>
       </View>
+
       <View style={styles.container}>
-        <Text style={styles.title}>Choose payment method</Text>
-        <PaymentMethodCard
-          imageSource={MtnLogo}
-          paymentMethodName="MTN Mobile Money"
-          isSelected={selectedCard === "MTN Mobile Money"}
-          onCardSelect={() => handleCardSelect("MTN Mobile Money")}
-        />
-        <PaymentMethodCard
-          imageSource={OrangeLogo}
-          paymentMethodName="Orange Money"
-          isSelected={selectedCard === "Orange Money"}
-          onCardSelect={() => handleCardSelect("Orange Money")}
-        />
-        <PaymentMethodCard
-          imageSource={StripeLogo}
-          paymentMethodName="Stripe Test Pay"
-          isSelected={selectedCard === "Stripe Test Pay"}
-          onCardSelect={() => handleCardSelect("Stripe Test Pay")}
-        />
-        <PaymentMethodCard
-          imageSource={VisaLogo}
-          paymentMethodName="Visa Card Pay"
-          isSelected={selectedCard === "Visa Card Pay"}
-          onCardSelect={() => handleCardSelect("Visa Card Pay")}
-        />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text style={styles.title}>Choose payment method</Text>
+          <PaymentMethodCard
+            imageSource={MtnLogo}
+            paymentMethodName="MTN Mobile Money"
+            isSelected={selectedCard === "MTN Mobile Money"}
+            onCardSelect={() => handleCardSelect("MTN Mobile Money")}
+          />
+          <PaymentMethodCard
+            imageSource={OrangeLogo}
+            paymentMethodName="Orange Money"
+            isSelected={selectedCard === "Orange Money"}
+            onCardSelect={() => handleCardSelect("Orange Money")}
+          />
+          
+          <PaymentMethodCard
+            imageSource={VisaLogo}
+            paymentMethodName="Visa Card Pay"
+            isSelected={selectedCard === "Visa Card Pay"}
+            onCardSelect={() => handleCardSelect("Visa Card Pay")}
+          />
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -76,24 +79,32 @@ const PaymentScreen = ({ route }) => {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
+    // backgroundColor: "#f5f5f5",
+    backgroundColor: "#070c35",
   },
   backgroundDiv: {
-    height: "17%",
+    height: "12%",
+    //height: "17%",
   },
   another: {
     height: "95%",
-    backgroundColor: "#070C35",
+    //backgroundColor: "#070C35",
+    //  backgroundColor: "red",
+
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
     padding: 15,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    //justifyContent:"center"
   },
   backArrowContainer: {
-    position: "absolute",
+    // position: "absolute",
     left: 15,
-    top: "29%",
-    transform: [{ translateY: -12 }],
-    zIndex: 1, // Ensure it's on top
+    //top: "29%",
+    // transform: [{ translateY: -12 }],
+    // zIndex: 1, // Ensure it's on top
   },
   backArrow: {
     width: 30,
@@ -101,7 +112,6 @@ const styles = StyleSheet.create({
     tintColor: "#CDD2F8",
   },
   header: {
-    textAlign: "center",
     color: "#CDD2F8",
     fontSize: 25,
     fontWeight: "900",
@@ -109,18 +119,24 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 15,
     paddingVertical: 25,
-    height: "93%",
-    marginTop: -(1.77 * StatusBar.currentHeight),
+    height: "90%",
+    position: 'relative',
+    // marginTop: -(0.77 * StatusBar.currentHeight),
     backgroundColor: "#fff",
+    //backgroundColor: "red",
+
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
   },
   title: {
     fontSize: 18,
     fontWeight: "bold",
-    alignSelf: 'center',
+    alignSelf: "center",
     marginBottom: 20,
-    color: '#070C35'
+    color: "#070C35",
+  },
+  heading: {
+    marginLeft: "20%",
   },
 });
 
